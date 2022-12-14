c651adc8a5b69409b6de44ac50e872f2f1993b74
apalermo01 <64085614+apalermo01@users.noreply.github.com>
2022-12-14 01:22:36 -0500

    Update vim.sh

--- setup/debian/vim.sh
+++ setup/debian/vim.sh
@@ -7,7 +7,7 @@ git clone https://github.com/morhetz/gruvbox.git
 cd gruvbox
 cp -r colors/* ~/.vim/colors/
 cp -r autoload/* ~/.vim/autoload/
-2
+
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
